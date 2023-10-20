44af6b5abbc9d2f29bd41bba7edbf05ea557411d
MandarUkrulkar <120402650+MandarUkrulkar@users.noreply.github.com>
2023-10-20 13:50:49 +0530

    added cvat.sh for automation on gcp

---
+++ cvat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt update
+sudo apt install git
+git --version
+
+
+#docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+
+
+#docker compose 
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
+git clone -b branch-v2.6.1 https://github.com/MandarUkrulkar/cvat-clone.git
+cd ./cvat-clone
+docker-compose up -d
